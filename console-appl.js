e67e7fa5b50bbe528799356065028ee96796f936
Rubanov Dmitri <rubanovdp@gmail.com>
2023-09-10 12:56:29 +0300

    first commit

---
+++ console-appl.js
@@ -0,0 +1,17 @@
+import promptSync from 'prompt-sync';
+import config from 'config';
+const prompt = promptSync({sigint: true});
+const guessNumber = 1+Math.trunc(Math.random() * 10);
+if(config.has('test') && config.get('test')){
+  console.log(guessNumber);
+}
+let running = true;
+do{
+  let num = prompt("guess number from 1 to 10");
+  if(guessNumber == num) {
+    running = false;
+    console.log('congratulations')
+  }else{
+    console.log('No. Try again')
+  }
+}while(running);
