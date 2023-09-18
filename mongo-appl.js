4b6dbf8efc5060b2e515b145156a770e5e3061aa
Rubanov Dmitri <rubanovdp@gmail.com>
2023-09-18 21:25:17 +0300

    add mongodb

---
+++ mongo-appl.js
@@ -0,0 +1,5 @@
+import MongoConnection from "./domain/MongoConnection.mjs";
+
+const mongoConnection = new MongoConnection(process.env.ATLAS_URI, 'college');
+const studentsMDB = mongoConnection.getCollection('students');
+studentsMDB.find({phone: {$regex:/^050/}}).toArray().stream().pipe(process.stdout);
--- view/DocTextView.js
+++
@@ -1,9 +0,0 @@
-export default class DocTextView{
-  renderLine(line){
-    return `<label style="font-size:1.5em;font-style:italic;display:block;text-align:center">${line}</label>`
-  }
-
-  renderError(error, response){
-    response.end(`<label style="font-size:1.5em;color:red;font-style:italic;display:block;text-align:center">${error}</label>`)
-  }
-}
